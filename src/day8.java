f0f4e2af8428e31d8668d712ec01b2e6be1bcdb0
supriyar25ai-commits <supriyar25ai@vetias.ac.in>
2026-06-13 15:37:51 +0530

    lang

--- src/day8.java
+++ src/day8.java
@@ -1,4 +1,5 @@
 public class day8 {
+    /*
      record courserecord(String dept,String name){
 
      }
@@ -7,4 +8,27 @@ static void main(){
          System.out.println(courserecord.dept());
          System.out.println(courserecord.name());
      }
+}
+
+     */
+   enum language {
+       EN ("English"),
+       GR ("German"),
+       JP ("japanese"),
+       TA ("Tamil");
+
+       String description;
+       language(String description){
+           this.description = description;
+
+       }
+   }
+
+   static void main(){
+   System.out.println("......language......");
+   System.out.println(language.EN.description);
+   System.out.println(language.GR.description);
+   System.out.println(language.JP.description);
+   System.out.println(language.TA.description);
+   }
 }
