a4c9b6384e3779e1ede249b9559a31f477e49983
supriya <supriyar25ai@vetias.ac.in>
2026-06-16 16:05:54 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -1,9 +1,12 @@
+/*
 import java.util.ArrayList;
+
 public class day10 {
+    /*
     public static void main(){
         ArrayList<String> students = new ArrayList<>();
         students.add("kumar");
-        students.add("devil");
+        students.add("devil");d
         students.add("Kavin");
         students.add("Ram");
         students.add("Arun");
@@ -12,9 +15,133 @@ public class day10 {
             System.out.println(s);
         }
         students.remove("Ram");
-        System.out.println("\nAfter Removing Ram:");
+        System.out.println("After Removing Ram:");
         for (String s : students) {
             System.out.println(s);
         }
     }
+
+
+
 }
+
+public class day10 {
+    static void presence(linked list<String> queue)
+        System.out.println("poo")
+      static void main(){
+          LinkedList<String> student = new Arraylist();
+          student.add("deva");
+          student.add("kumar");
+          student.add("sam");
+          student.a
+      }
+
+}
+
+ */
+
+    /*
+    public class day10 {
+        static void main() {
+            LinkedList<String> queue = new LinkedList<String>();
+            queue.add("ram");
+            queue.add("sam");
+            queue.add("akash");
+            queue.add("shri");
+            queue.add("deva");
+            while(!queue.isEmpty()) {
+                process(queue);
+            }
+        }
+        static void process(LinkedList<String> queue){
+            String removed=queue.removeFirst();
+            System.out.println("Removed:"+removed);
+
+        }
+    }
+
+     */
+    /*
+    static void main() {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("kumar");
+        list.add("ravi");
+        list.add("kiran");
+        list.add("bharanii");
+        list.add("ram");
+        while(!list.isEmpty()) {
+            process(list);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
+    }
+}
+
+     */
+/*
+import java.util.ArrayList;
+
+public class day10 {
+    static void process(LinkedList<String> List) {
+        System.out.println("removed:" + List.removeFirst());
+    }
+}
+    static void main() {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        while (list.size() > 1) {
+            process(list);
+        }
+    }
+
+ */
+    import java.util.LinkedList;
+public class Day10 {
+    static void process(LinkedList<String> queue) {
+        System.out.println("Removed:" + queue.removeFirst());
+    }
+
+    static void main() {
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        queue.add("E");
+
+        while (queue.size() > 1) {
+            process(queue);
+        }
+    }
+}
+static void process(LinkedList<String>List) {
+    System.out.println("removed:" + List.removeFirst());
+}
+static void main() {
+    LinkedList<String> list = new LinkedList<>();
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.add("d");
+    list.add("e");
+    while (list.size() > 1) {
+        process(list);
+    }
+}
+
+
+
+
+
+
+
+
+
+
